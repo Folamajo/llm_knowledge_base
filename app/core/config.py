41044220b8ad510979e05b9d7ca4f2050a64280e
fmajoyegbe@gmail.com <fmajoyegbe@gmail.com>
2025-06-30 10:42:31 +0100

    set up config.py file

--- app/core/config.py
+++ app/core/config.py
@@ -3,4 +3,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Settings:
+   JWT_SECRET_KEY = os.getenv("SECRET_KEY")
+   DB_CONNECTION = os.getenv("DATABASE_CONNECTION")
+   TOKEN_EXP_DATE = int(os.getenv("TOKEN_EXPIRATION_DATE"))
+   HASHING_ALGORITHM = os.getenv("HASHING_ALGORITHM")
    
